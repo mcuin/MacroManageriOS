2fe31c884c11eb14618e3aaf28e4e0282a665cde
mcuin <mykcuin@gmail.com>
2017-08-26 09:15:06 -0600

    Initial Commit

---
+++ MacrosManageriOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MacrosManageriOS
+//
+//  Created by Mykal Cuin on 8/26/17.
+//  Copyright © 2017 Mykal Cuin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
