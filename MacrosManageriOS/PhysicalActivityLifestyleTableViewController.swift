fb472c3b1a2c5c2fc79bad3acfcb9559e7d266d2
mcuin <mykcuin@gmail.com>
2017-10-24 10:47:18 -0600

    Physical activity lifestyle tableview.

--- MacrosManageriOS/PhysicalActivityLifestyleTableViewController.swift
+++ MacrosManageriOS/PhysicalActivityLifestyleTableViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 
 class PhysicalActivityLifestyleTableViewController: UITableViewController {
     
+    let physicalActivityLifestyleArray = ["Sedentary Adult", "Adult Recreational Exerciser", "Adult Competitive Athlete", "Adult Building Muscle", "Dieting Athlete", "Teenage Growing Athelete"]
+    
     @IBOutlet var physicalActivityLifestyleTableView: UITableView!
     
     override func viewDidLoad() {
@@ -30,6 +32,51 @@ class PhysicalActivityLifestyleTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 0
+        return physicalActivityLifestyleArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "physicalActivityLiftstyleCell")
+        
+        cell?.textLabel?.text = physicalActivityLifestyleArray[indexPath.row]
+        
+        return cell!
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        
+        switch indexPath.row {
+        case 0:
+            
+            UserDefaults.standard.set("sedentaryAdult", forKey: "physicalAcitvityLifestyle")
+        case 1:
+            
+            UserDefaults.standard.set("adultRecreationalExerciser", forKey: "physicalAcitvityLifestyle")
+
+        case 2:
+            
+            UserDefaults.standard.set("adultCompetitiveAthlete", forKey: "physicalAcitvityLifestyle")
+
+        case 3:
+            
+            UserDefaults.standard.set("adultBuildingMuscle", forKey: "physicalAcitvityLifestyle")
+        case 4:
+            
+            UserDefaults.standard.set("dietingAthlete", forKey: "physicalAcitvityLifestyle")
+        case 5:
+            
+            UserDefaults.standard.set("teenageGrowingAthlete", forKey: "physicalAcitvityLifestyle")
+        default:
+            
+            UserDefaults.standard.set("sedentary", forKey: "physicalAcitvityLifestyle")
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        
+        tableView.cellForRow(at: indexPath)?.accessoryType = .none
     }
 }
