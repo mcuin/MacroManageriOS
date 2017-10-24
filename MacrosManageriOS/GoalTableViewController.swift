ecce4d452665d8248cf12e337905dd5b0df5f2a9
mcuin <mykcuin@gmail.com>
2017-10-24 11:00:48 -0600

    Goal tableview

--- MacrosManageriOS/GoalTableViewController.swift
+++ MacrosManageriOS/GoalTableViewController.swift
@@ -11,8 +11,9 @@ import UIKit
 
 class GoalTableViewController: UITableViewController {
     
-    @IBOutlet var goalTableView: UITableView!
+    let goalsArray = ["Burn Fat Suggested (-15%)", "Burn Fat Aggressive (-20%)", "Burn Fat Reckless (-25%)", "Maintain (+/-0%)", "Build Muscle Suggested (+5%)", "Build Muscle Aggressive (+10%)", "Build Muscle Reckless (+15%)"]
     
+    @IBOutlet var goalTableView: UITableView!
     
     override func viewDidLoad() {
         
@@ -22,5 +23,61 @@ class GoalTableViewController: UITableViewController {
     override func didReceiveMemoryWarning() {
         
         super.didReceiveMemoryWarning()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return goalsArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell")
+        
+        cell?.textLabel?.text = goalsArray[indexPath.row]
+        
+        return cell!
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        
+        switch indexPath.row {
+        case 0:
+            
+            UserDefaults.standard.set("burnFatSuggested", forKey: "goal")
+        case 1:
+            
+            UserDefaults.standard.set("burnFatAggressive", forKey: "goal")
+        case 2:
+            
+            UserDefaults.standard.set("burnFatReckless", forKey: "goal")
+        case 3:
+            
+            UserDefaults.standard.set("maintain", forKey: "goal")
+        case 4:
+            
+            UserDefaults.standard.set("buildMuscleSuggested", forKey: "goal")
+        case 5:
+            
+            UserDefaults.standard.set("buildMuscleAggressive", forKey: "goal")
+        case 6:
+            
+            UserDefaults.standard.set("buildMuscleReckless", forKey: "goal")
+        default:
+            
+            UserDefaults.standard.set("maintain", forKey: "goal")
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        
+        tableView.cellForRow(at: indexPath)?.accessoryType = .none
     }
 }
