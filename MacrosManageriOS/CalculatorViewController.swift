c12abc4eb64c000df528dc60b4d6ff98da051456
mcuin <mykcuin@gmail.com>
2018-01-25 15:41:38 -0700

    Calculator is now calculating and storing daily calories.

--- MacrosManageriOS/CalculatorViewController.swift
+++ MacrosManageriOS/CalculatorViewController.swift
@@ -22,25 +22,17 @@ class CalculatorViewController: UIViewController, UITabBarDelegate, UITableViewD
     var kilogramsTextView: UITextView!
     var stoneTextField: UITextField!
     var stoneTextView: UITextView!
+    var weightMeasure = ""
     
     let actvityFactrosArray = ["Daily Activity Level", "Physical Activity Lifestyle", "Fitness Goal"]
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
-    }
-    
-    override func didReceiveMemoryWarning() {
-        
-        super.didReceiveMemoryWarning()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        
-        super.viewWillAppear(animated)
         
         if let birthDate = UserDefaults.standard.value(forKey: "birthDate"), let gender = UserDefaults.standard.value(forKey: "gender"), let weightMeasure = UserDefaults.standard.value(forKey: "weightMeasure"), let feet = UserDefaults.standard.value(forKey: "feet"), let inches = UserDefaults.standard.value(forKey: "inches"), let cm = UserDefaults.standard.value(forKey: "cm") {
             
+            self.weightMeasure = weightMeasure as! String
             weightTextView = UITextView(frame: CGRect(x: 16, y: 16, width: 75, height: 25))
             weightTextView.text = "Weight"
             weightTextView.textColor = .black
@@ -49,44 +41,68 @@ class CalculatorViewController: UIViewController, UITabBarDelegate, UITableViewD
             switch(weightMeasure as! String) {
                 
             case "imperial":
-            
-                poundsTextField = UITextField(frame: CGRect(x: 16, y: weightTextView.bounds.height + 16, width: 1, height: 1))
+                
+                poundsTextField = UITextField(frame: CGRect(x: 16, y: weightTextView.frame.maxY + 16, width: 1, height: 1))
                 poundsTextField.placeholder = "Pounds"
                 poundsTextField.sizeToFit()
                 previousView = poundsTextField
                 calculatorScrollView.addSubview(poundsTextField)
             case "metric":
                 
-                kilogramsTextField = UITextField(frame: CGRect(x: 16, y: weightTextView.bounds.height + 16, width: 1, height: 1))
+                kilogramsTextField = UITextField(frame: CGRect(x: 16, y: weightTextView.frame.maxY + 16, width: 1, height: 1))
                 kilogramsTextField.placeholder = "Kilograms"
                 kilogramsTextField.sizeToFit()
                 calculatorScrollView.addSubview(kilogramsTextField)
                 previousView = kilogramsTextField
             case "stone":
                 
-                stoneTextField = UITextField(frame: CGRect(x: 16, y: weightTextView.bounds.height + 16, width: 1, height: 1))
+                stoneTextField = UITextField(frame: CGRect(x: 16, y: weightTextView.frame.maxY + 16, width: 1, height: 1))
                 stoneTextField.placeholder = "Stones"
                 stoneTextField.sizeToFit()
                 calculatorScrollView.addSubview(stoneTextField)
                 previousView = stoneTextField
             default:
                 
-                poundsTextField = UITextField(frame: CGRect(x: 16, y: weightTextView.bounds.height + 16, width: 1, height: 1))
+                poundsTextField = UITextField(frame: CGRect(x: 16, y: weightTextView.frame.maxY + 16, width: 1, height: 1))
                 poundsTextField.placeholder = "Pounds"
                 poundsTextField.sizeToFit()
                 calculatorScrollView.addSubview(poundsTextField)
                 previousView = poundsTextField
             }
             
-            print(previousView.bounds.height)
-            
             let actvityFactorsTableView = UITableView()
+            actvityFactorsTableView.isScrollEnabled = false
             actvityFactorsTableView.register(UITableViewCell.self, forCellReuseIdentifier: "activityFactorCell")
             actvityFactorsTableView.delegate = self
             actvityFactorsTableView.dataSource = self
             
-            actvityFactorsTableView.frame = CGRect(x: 0, y: self.previousView.bounds.height, width: UIScreen.main.bounds.width, height: 200)
+            actvityFactorsTableView.frame = CGRect(x: 0, y: self.previousView.frame.maxY + 16, width: UIScreen.main.bounds.width, height: 135)
             calculatorScrollView.addSubview(actvityFactorsTableView)
+            
+            let prefferedFatTextView = UITextView()
+            
+            prefferedFatTextView.text = "Preffered Percent of Fat in Diet"
+            prefferedFatTextView.sizeToFit()
+            prefferedFatTextView.frame = CGRect(x: 16, y: actvityFactorsTableView.frame.maxY + 16, width: prefferedFatTextView.contentSize.width, height: prefferedFatTextView.contentSize.height)
+            
+            calculatorScrollView.addSubview(prefferedFatTextView)
+            
+            let prefferedFatTextField = UITextField()
+            
+            prefferedFatTextField.placeholder = "Percent"
+            prefferedFatTextField.keyboardType = .decimalPad
+            prefferedFatTextField.frame = CGRect(x: 16, y: prefferedFatTextView.frame.maxY + 4, width: 75, height: 50)
+            
+            calculatorScrollView.addSubview(prefferedFatTextField)
+            
+            let calculateButton = UIButton(frame: CGRect(x: UIScreen.main.bounds.maxX - 91, y: prefferedFatTextField.frame.maxY + 16, width: 1, height: 1))
+            calculateButton.setTitle("Calculate", for: .normal)
+            calculateButton.setTitleColor(.blue, for: .normal)
+            calculateButton.sizeToFit()
+            calculateButton.addTarget(self, action: #selector(calculate), for: .touchUpInside)
+            //calculateButton.frame =
+            
+            calculatorScrollView.addSubview(calculateButton)
         } else {
             
             let calculatorAlertController = UIAlertController(title: "Missing Data", message: "Please fill out your data in the settings screen.", preferredStyle: .alert)
@@ -114,6 +130,18 @@ class CalculatorViewController: UIViewController, UITabBarDelegate, UITableViewD
                 self.present(calculatorAlertController, animated: true, completion: nil)
             }
         }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+       
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -151,6 +179,161 @@ class CalculatorViewController: UIViewController, UITabBarDelegate, UITableViewD
         default:
             
             break
+        }
+    }
+    
+    @objc func calculate() {
+        
+        var bmr: Double!
+        var tdee: Double!
+        var calories: Double!
+        var protein: Double!
+        var proteinCalories: Double!
+        var fat: Double!
+        var fatCalories: Double!
+        var carbs: Double!
+        var carbsCalories: Double!
+        let calendar = NSCalendar.current
+        let currentDate = NSDate()
+        let birthDate = UserDefaults.standard.value(forKey: "birthDate") as! NSDate
+        let components = Calendar.current.dateComponents([.year], from: birthDate as Date, to: Date())
+        let age = components.year
+        
+        switch weightMeasure {
+        case "imperial":
+            
+            let pounds = Double(poundsTextField.text!)
+            let kg = pounds! * 0.454
+            let stone = (pounds! / 14)
+            
+            UserDefaults.standard.set(pounds, forKey: "pounds")
+            UserDefaults.standard.set(kg, forKey: "kilograms")
+            UserDefaults.standard.set(stone, forKey: "stone")
+            
+        case "metric":
+            
+            let kg = Double(kilogramsTextField.text!)
+            let pounds = kg! / 0.454
+            let stone = kg! * 0.157
+            
+            UserDefaults.standard.set(kg, forKey: "kilograms")
+            UserDefaults.standard.set(pounds, forKey: "pounds")
+            UserDefaults.standard.set(stone, forKey: "stone")
+            
+        case "stone":
+            
+            let stone = Double(stoneTextField.text!)
+            let pounds = stone! * 14
+            let kg = stone! * 6.35
+            
+            UserDefaults.standard.set(stone, forKey: "stone")
+            UserDefaults.standard.set(pounds, forKey: "kilograms")
+            UserDefaults.standard.set(kg, forKey: "kilograms")
+        default:
+            
+            let weightMeasureAlert = UIAlertController(title: "Weight Error", message: "There's was an error with your weight entry. Please try again.", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: { (void) in
+                weightMeasureAlert.dismiss(animated: true, completion: nil)
+                })
+            
+            weightMeasureAlert.addAction(okAction)
+            
+            self.present(weightMeasureAlert, animated: true, completion: nil)
+        }
+        
+        if let kg = UserDefaults.standard.value(forKey: "kilograms") as? Double, let cm = UserDefaults.standard.value(forKey: "cm") as? Double {
+            
+            if let gender = UserDefaults.standard.value(forKey: "gender") {
+                
+                switch(gender as! String) {
+                    
+                case "male":
+                 
+                    let weightCalc = (10 * kg)
+                    let heightCalc = (6.25 * cm)
+                    let ageCalc = Double(5 * age!)
+                    bmr = weightCalc + heightCalc - ageCalc + 5
+                    
+                case "female":
+                    
+                    let weightCalc = (10 * kg)
+                    let heightCalc = (6.25 * cm)
+                    let ageCalc = Double(5 * age!)
+                    bmr = weightCalc + heightCalc - ageCalc - 161
+                    print(bmr)
+                default:
+                    break
+                }
+            }
+            
+            if bmr != nil {
+                
+                if let dailyActivity = UserDefaults.standard.value(forKey: "dailyActivityLevel") as? String {
+                    
+                    switch(dailyActivity) {
+                        
+                    case "veryLight":
+                        
+                        tdee = bmr * 1.20
+                    case "light":
+                        
+                        tdee = bmr * 1.45
+                    case "moderate":
+                        
+                        tdee = bmr * 1.55
+                    case "heavy":
+                        
+                        tdee = bmr * 1.75
+                    case "veryHeavy":
+                        
+                        tdee = bmr * 2.00
+                    default:
+                        
+                        tdee = bmr * 1.20
+                    }
+                }
+                
+                print(tdee)
+            }
+            
+            if tdee != nil {
+                
+                if let goal = UserDefaults.standard.value(forKey: "goal") as? String {
+                    
+                    switch(goal) {
+                        
+                    case "maintain":
+                        
+                        calories = tdee
+                    case "burnFatSuggested":
+                        
+                        calories = tdee - (tdee * 0.15)
+                    case "burnFatAggressive":
+                        
+                        calories = tdee - (tdee * 0.20)
+                        
+                    case "burnFatReckless":
+                        
+                        calories = tdee - (tdee * 0.25)
+                    case "buildMuscleSuggested":
+                        
+                        calories = tdee + (tdee * 0.05)
+                    case "buildMuscleAggressive":
+                        
+                        calories = tdee + (tdee * 0.10)
+                    case "buildMuscleReckless":
+                        
+                        calories = tdee + (tdee * 0.15)
+                    default:
+                        
+                        calories = tdee
+                    }
+                }
+                
+                print(calories)
+                UserDefaults.standard.set(calories, forKey: "calories")
+            }
         }
     }
 }
