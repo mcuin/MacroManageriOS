6f71ce1a967da19ed80864db10094fa18cf93378
mcuin <mykcuin@gmail.com>
2018-03-13 15:37:07 -0600

    Layout and class for adding food.

---
+++ MacrosManageriOS/AddFoodViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AddFoodViewController.swift
+//  MacrosManageriOS
+//
+//  Created by Mykal Cuin on 3/13/18.
+//  Copyright © 2018 Mykal Cuin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddFoodViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
