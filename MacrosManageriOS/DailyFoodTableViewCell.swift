a985d4ebb0abafe33d93eb12414683613b1334da
mcuin <mykcuin@gmail.com>
2018-03-13 15:08:54 -0600

    Added in table cell data for food list.

---
+++ MacrosManageriOS/DailyFoodTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  DailyFoodTableViewCell.swift
+//  MacrosManageriOS
+//
+//  Created by Mykal Cuin on 3/6/18.
+//  Copyright © 2018 Mykal Cuin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DailyFoodTableViewCell: UITableViewCell {
+    @IBOutlet weak var foodNameLabel: UILabel!
+    @IBOutlet weak var foodServingLabel: UILabel!
+    @IBOutlet weak var foodCaloriesLabel: UILabel!
+}
