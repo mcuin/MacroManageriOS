022f631907ceb7fb49dafbeebf183f09bc2ccfba
mcuin <mykcuin@gmail.com>
2018-11-14 19:56:02 -0700

    Sign up validations working correctly.

--- MacrosManageriOS/SignUpViewController.swift
+++ MacrosManageriOS/SignUpViewController.swift
@@ -27,4 +27,72 @@ class SignUpViewController: UIViewController {
         
         super.didReceiveMemoryWarning()
     }
+    @IBAction func signUpButtonAction(_ sender: UIButton) {
+        
+        if signUpFirstNameTextField.text == "" {
+            
+            let firstNameAlert = UIAlertController(title: "First Name Error", message: "Please enter your first name.", preferredStyle: .alert)
+            firstNameAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            DispatchQueue.main.async {
+                
+                self.present(firstNameAlert, animated: true, completion: nil)
+            }
+        } else if signUpLastNameTextField.text == "" {
+            
+            let lastNameAlert = UIAlertController(title: "Last Name Error", message: "Please enter your last name.", preferredStyle: .alert)
+            lastNameAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            DispatchQueue.main.async {
+                
+                self.present(lastNameAlert, animated: true, completion: nil)
+            }
+        } else if signUpEmailTextField.text == "" || !self.isValidEmail(testStr: signUpEmailTextField.text!){
+            
+            let emailAlert = UIAlertController(title: "Email Error", message: "Please enter a valid email.", preferredStyle: .alert)
+            emailAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            DispatchQueue.main.async {
+                
+                self.present(emailAlert, animated: true, completion: nil)
+            }
+        } else if signUpPasswordTextField.text == "" {
+            
+            let passwordAlert = UIAlertController(title: "Password Error", message: "Please enter a password.", preferredStyle: .alert)
+            passwordAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            DispatchQueue.main.async {
+                
+                self.present(passwordAlert, animated: true, completion: nil)
+            }
+        } else if signUpConfirmPasswordTextField.text == "" {
+            
+            let confirmPasswordAlert = UIAlertController(title: "Confirm Password Error", message: "Please confirm you password.", preferredStyle: .alert)
+            confirmPasswordAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            DispatchQueue.main.async {
+                
+                self.present(confirmPasswordAlert, animated: true, completion: nil)
+            }
+        } else if signUpPasswordTextField.text != signUpConfirmPasswordTextField.text {
+            
+            let passwordMatchAlert = UIAlertController(title: "Password Error", message: "The entered passwords do not match.", preferredStyle: .alert)
+            passwordMatchAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            DispatchQueue.main.async {
+                
+                self.present(passwordMatchAlert, animated: true, completion: nil)
+            }
+        } else {
+            
+            print("Sign up working")
+        }
+    }
+    
+    func isValidEmail(testStr:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: testStr)
+    }
 }
