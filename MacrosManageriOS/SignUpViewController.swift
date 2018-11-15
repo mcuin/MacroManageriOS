0fc54e7cdb57cd43b827284d09547bacaac1a67e
mcuin <mykcuin@gmail.com>
2018-11-14 18:30:56 -0700

    Initial sign up view controller

---
+++ MacrosManageriOS/SignUpViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SignUpViewController.swift
+//  MacrosManageriOS
+//
+//  Created by Mykal Cuin on 11/14/18.
+//  Copyright © 2018 Mykal Cuin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        
+        super.didReceiveMemoryWarning()
+    }
+}
